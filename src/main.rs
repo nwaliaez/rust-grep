7a45b7d82c551915aa4dedf6dc62dd26b938ff66
Abhishek Saini <122618032+AbhishekkSaini@users.noreply.github.com>
2024-02-06 18:28:05 +0530

    Update main.rs
    >>>>>>>.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-use std::env;
-use std::process;
-
+use std::{env, process};
 use minigrep::Config;
 
-fn main(){
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
+fn main() {
+    let config = Config::from_args().unwrap_or_else(|err| {
+        eprintln!("Error: {}", err);
         process::exit(1);
     });
-    
-    if let Err(e) = minigrep::run(config){
 
-        dbg!("{config}");
-        eprintln!("Application error: {e}");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
-    
 }
+
